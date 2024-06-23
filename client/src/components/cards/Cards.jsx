37ebc285758f9b4addc2bb22fc82e0f25bc83c22
bodyraul <aurelien.peria@gmail.com>
2024-06-23 18:34:22 +0200

    v6 responsive

--- client/src/components/cards/Cards.jsx
+++ client/src/components/cards/Cards.jsx
@@ -58,9 +58,9 @@ export default function Cards({setcolor}) {
   const rotationCardTrois = useTransform(scrollYProgress,[0.6,0.95],[`translate3d(-110px,115px,0px) rotateZ(10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
   const ColorTitre= useTransform(scrollYProgress,[0,0.40,0.41666,0.50,0.58],[`#181818`,`#181818`,`#4F4F4F`,`#B2B2B2`,`#FBFBFB`]);
   const opacityTitre= useTransform(scrollYProgress,[0,0.6,0.8,0.85,0.9],[1,1,0.7,0.4,0]);
-  const rotationCardUneResponsive= useTransform(scrollYProgress,[0.4,0.6],[`translate3d(0px,0px,0px) rotateZ(10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
-  const rotationCardDeuxResponsive= useTransform(scrollYProgress,[0.6,0.8],[`translate3d(0px,0px,0px) rotateZ(10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
-  const rotationCardTroisResponsive= useTransform(scrollYProgress,[0.8,1],[`translate3d(0px,0px,0px) rotateZ(10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
+  const rotationCardUneResponsive= useTransform(scrollYProgress,[0.4,0.5],[`translate3d(0px,0px,0px) rotateZ(10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
+  const rotationCardDeuxResponsive= useTransform(scrollYProgress,[0.5,0.6],[`translate3d(0px,0px,0px) rotateZ(-10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
+  const rotationCardTroisResponsive= useTransform(scrollYProgress,[0.75,0.85],[`translate3d(0px,0px,0px) rotateZ(10deg)`,`translate3d(0px,0px,0px) rotateZ(0deg)`]);
 
 
   return (
--- client/src/components/defilementPersonnel/defilementPersonnel.jsx
+++ client/src/components/defilementPersonnel/defilementPersonnel.jsx
@@ -8,7 +8,8 @@ import imgElysa from '../../assets/img/elysa.jpeg'
 import imgCassandra from '../../assets/img/casandra.jpeg'
 import imgQuotation from '../../assets/img/quotation.png'
 import { useRef } from 'react'
-
+import { useState } from 'react'
+import { useEffect } from 'react'
 import { motion, useScroll,useTransform, useMotionValueEvent } from "framer-motion"
 
 export default function DefilementPersonnel({setcolor}) {
@@ -34,55 +35,74 @@ export default function DefilementPersonnel({setcolor}) {
     }
     })
 
-    const translateDefilement = useTransform(scrollYProgress,[0.38,1],["translateX(0vw)","translateX(-130vw)"]);
-    const transformImgUne = useTransform(scrollYProgress,[0.10,0.35],["translate3d(0,-50px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
-    const transformImgDeux = useTransform(scrollYProgress,[0.10,0.35],["translate3d(0,-130px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
-    const transformContainerUn = useTransform(scrollYProgress,[0.10,0.35],["translate3d(0px,230px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
-    const transformContainerDeux = useTransform(scrollYProgress,[0.10,0.35],["translate3d(60px,230px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
-    const opacityParaUn = useTransform(scrollYProgress,[0.40,0.60],[1,0]);
-    const opacityParaDeux = useTransform(scrollYProgress,[0.40,0.45,0.80,1],[0,1,1,0]);
-    const opacityParaTrois = useTransform(scrollYProgress,[0.72,0.80],[0,1]);
+    const transformImgUne = useTransform(scrollYProgress,[0.10,0.27],["translate3d(0,-50px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
+    const transformImgDeux = useTransform(scrollYProgress,[0.10,0.26],["translate3d(0,-130px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
+    const transformContainerUn = useTransform(scrollYProgress,[0.10,0.25],["translate3d(0px,230px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
+    const transformContainerDeux = useTransform(scrollYProgress,[0.10,0.28],["translate3d(60px,230px,0px) rotateZ(-20deg)","translate3d(0px,0px,0px) rotateZ(0deg)"]);
+    const defilementParaUn = useTransform(scrollYProgress,[0.30,1],["translateX(-50%) translateY(-50%)","translateX(-290%) translateY(-50%)"]);
+    const defilementParaDeux = useTransform(scrollYProgress,[0.30,1],["translateX(70%) translateY(-50%)","translateX(-170%) translateY(-50%)"]);
+    const defilementParaTrois = useTransform(scrollYProgress,[0.30,1],[" translateX(190%) translateY(-50%)","translateX(-50%) translateY(-50%)"]);
+    const opacityParaUn = useTransform(scrollYProgress,[0.45,0.55],[1,0]);
+    const opacityParaDeux = useTransform(scrollYProgress,[0.45,0.55,0.8,0.9],[0,1,1,0]);
+    const opacityParaTrois = useTransform(scrollYProgress,[0.8,0.9],[0,1]);
 
-  return (
-    <div ref={ref} className='partieDefilementPersonnel'>
-        <div className="allPersonnel">
-            <motion.div style={{transform:translateDefilement}} className='defilementPersonnel'>
-                <motion.div style={{opacity:opacityParaUn}} className='ElementPersonnel'>
-                    <motion.div style={{transform:transformContainerUn,transition: "all 0.1s  linear"}} className='partieUne'>
+    const [matches, setMatches] = useState(
+        window.matchMedia("(max-width: 1350px)").matches
+      )
+
+    
+    useEffect(() => {
+    function eventResponsive(e){
+        setMatches( e.matches );
+    }
+
+    window.matchMedia("(max-width: 1350px)").addEventListener('change', eventResponsive);
+    
+    return () => {
+        window.matchMedia("(max-width: 1350px)").removeEventListener('change', eventResponsive);
+    }
+    }, [])
+
+    if(!matches){
+        return(
+         <div ref={ref}  className='partieDefilementPersonnel'>
+            <div className='allPersonnel'>
+                <motion.div style={{transform:defilementParaUn,opacity:opacityParaUn}} className='ElementPersonnelUn'>
+                    <motion.div style={{transform:transformContainerUn,transition: "all 0.1s  linear"}} className='partieUne'>		
                         <div className='partieUneImg'>
                             <img src={imgQuotation} alt="" />
                         </div>
-                        <motion.div  className='partieUneDescription'>
+                        <div  className='partieUneDescription'>
                             <span>Awork(life)saving app.</span><br></br><span>We saved 40% of our</span><br></br><span>time merchandising</span><br></br><span>every month.</span>
-                        </motion.div>
+                        </div>
                     </motion.div>
                     <div className='partieDeux'>
                         <div className='partiePhotoTitre'>
                             <motion.div style={{transform:transformImgUne,transition: "all 0.1s  linear"}} className='partiePhotoPersonnel'>
-                                 <img  src={imgAnnie} alt="" />   
+                                    <img  src={imgAnnie} alt="" />   
                             </motion.div>
-                            <motion.div style={{transform:transformImgDeux,transition: "all 0.1s  linear"}}  className='partieTitrePersonnel'>
+                            <motion.div style={{transform:transformImgDeux,transition: "all 0.1s  linear"}} className='partieTitrePersonnel'>
                                 <img src={imgStronger} alt="" />
                             </motion.div>
                         </div>
-                        <motion.div style={{transform:transformContainerDeux,transition: "all 0.1s  linear"}} className='partiePersonnelNom'>
+                        <motion.div style={{transform:transformContainerDeux,transition: "all 0.1s  linear"}}className='partiePersonnelNom'>
                             <span>Hanny Eldblom</span><br></br> <span>Head of Ecommerce</span>
                         </motion.div>
                     </div>
                 </motion.div>
-                <motion.div style={{opacity:opacityParaDeux}}  className='ElementPersonnel'>
-                    <div className='partieUne'>
+                <motion.div style={{transform:defilementParaDeux,opacity:opacityParaDeux}} className='ElementPersonnelDeux'>
+                    <div className='partieUne'>		
                         <div className='partieUneImg'>
-                            <img src={imgQuotation} alt="" />
+                            <img src={imgQuotation}alt="" />
                         </div>
-                        <div className='partieUneDescription'>
+                        <div  className='partieUneDescription'>
                             <span>Time-effective visual</span><br></br><span>merchandising, Depict is</span><br></br><span>super user-friendly and</span><br></br><span>smart.</span>
                         </div>
                     </div>
                     <div className='partieDeux'>
                         <div className='partiePhotoTitre'>
-                            <div className='partiePhotoPersonnel'>
-                                 <img src={imgElysa} alt="" />   
+                            <div  className='partiePhotoPersonnel'>
+                                    <img  src={imgElysa} alt="" />   
                             </div>
                             <div className='partieTitrePersonnel'>
                                 <img src={imgEytis} alt="" />
@@ -93,19 +113,19 @@ export default function DefilementPersonnel({setcolor}) {
                         </div>
                     </div>
                 </motion.div>
-                <motion.div style={{opacity:opacityParaTrois}} className='ElementPersonnel'>
-                    <div className='partieUne'>
+                <motion.div style={{transform:defilementParaTrois,opacity:opacityParaTrois}} className='ElementPersonnelTrois'>
+                    <div className='partieUne'>		
                         <div className='partieUneImg'>
                             <img src={imgQuotation} alt="" />
                         </div>
-                        <div className='partieUneDescription'>
+                        <div  className='partieUneDescription'>
                             <span>Depict has</span><br></br><span>revolutionized our</span><br></br><span>workflow with AI</span><br></br><span>capabilities to enhance</span><br></br><span>our operations.</span>
                         </div>
                     </div>
                     <div className='partieDeux'>
                         <div className='partiePhotoTitre'>
-                            <div className='partiePhotoPersonnel'>
-                                 <img src={imgCassandra} alt="" />   
+                            <div  className='partiePhotoPersonnel'>
+                                    <img  src={imgCassandra}  alt="" />   
                             </div>
                             <div className='partieTitrePersonnel'>
                                 <img src={imgUnderstatement} alt="" />
@@ -116,9 +136,33 @@ export default function DefilementPersonnel({setcolor}) {
                         </div>
                     </div>
                 </motion.div>
-                
-            </motion.div>
+            </div>
         </div>
-    </div>
-  )
+        )
+    }
+
+    if(matches){
+        return(
+            <div className='allResponsiveMarc'>
+             <div className='responsiveMarchandising'>
+                <div className='responsivePartieIntro'>
+                    <img src={imgQuotation}alt="" />
+                    <p>A work(like)saving app. We saved 40% of our time merchandising every month. </p>
+                </div>
+                <div className='responsivePartieNom'>
+                    <span>Hanny Eldblom</span><br></br><span>Head of Ecommerce</span>
+                </div>  
+                <div className='responsivePartieImage'>
+                    <div className='responsiveImg'>
+                        <img className='imgCassandra' src={imgElysa} alt="" />
+                        <div className='testImg'> 
+                            <img src={imgStronger} alt="" />
+                        </div>
+                    </div>
+                    <img id='imgResUn' src={imgAnnie} alt="" />
+                </div>  
+             </div>
+            </div>
+        )
+    }
 }
